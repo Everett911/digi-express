67064063002dbe42c7975334f55dbf6ba8bd95d2
Everett911 <everett.lara1697@gmail.com>
2026-08-24 19:04:52 +0800

    s

--- src/app/(main)/not_found.tsx
+++ src/app/(main)/not_found.tsx
@@ -5,15 +5,9 @@ import "./not_found.module.css";
 export function NotFoundPage({ totalQuantity }: { totalQuantity: string }) {
   return (
     <>
-      {/* You can choose whatever title and favicon you want. */}
       <title>404 Page Not Found</title>
       <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
 
-      {/* Remember to add the <Header> so it looks like it's
-      on the same website. */}
-      <Header totalQuantity={totalQuantity} />
-
-      {/* You can style this message however you want. */}
       <div className="not-found-message">Page not found</div>
       <Footer />
     </>
