9f8e48a1c5bf26bd60d4c8f3b5e7706d59372dfe
Everett911 <everett.lara1697@gmail.com>
2026-08-25 22:09:14 +0800

    prisma problem

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/prisma/generated/client";
+import { PrismaClient } from "@prisma/client";
 import { betterAuth, BetterAuthOptions } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { nextCookies } from "better-auth/next-js";
