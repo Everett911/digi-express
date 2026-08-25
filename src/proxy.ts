28b4a2d196ca0f97e2cbda12c0c5fb96cfe70e26
Everett911 <everett.lara1697@gmail.com>
2026-08-25 19:07:40 +0800

    middleware to proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { pathname } = new URL(request.url);
 
   if (!pathname.startsWith("/admin")) {
