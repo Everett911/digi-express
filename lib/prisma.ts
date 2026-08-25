0698be700daebdf04fe7cb1f9e14f2e0342ab4ce
Everett911 <everett.lara1697@gmail.com>
2026-08-25 17:32:32 +0800

    adding

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,7 +4,17 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/prisma/generated/client";
 import { Pool } from "pg";
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
 const adapter = new PrismaPg(pool);
 
 const globalForPrisma = globalThis as unknown as {
