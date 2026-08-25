a0a13e936c0302410d3bc6ee1ade558805506c4d
Everett911 <everett.lara1697@gmail.com>
2026-08-25 19:20:42 +0800

    fix the page

--- src/app/(main)/auth/page.tsx
+++ src/app/(main)/auth/page.tsx
@@ -1,11 +1,34 @@
 import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
 import AuthClientPage from "./auth";
+import { Suspense } from "react";
+import { LoaderCircleIcon } from "lucide-react";
+
+const AuthClientPage = dynamic(() => import("./AuthClientPage"), {
+  ssr: false,
+});
 
 export default async function AuthPage() {
   await auth.api.getSession({
     headers: await headers(),
   });
 
-  return <AuthClientPage />;
+  return (
+    <Suspense
+      fallback={
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          <LoaderCircleIcon size={40} />
+        </div>
+      }
+    >
+      <AuthClientPage />
+    </Suspense>
+  );
 }
