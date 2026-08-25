3ff35de89ae0a2b59f849925e0567c17ebc5d9fe
Everett911 <everett.lara1697@gmail.com>
2026-08-25 14:32:27 +0800

    removing the http because i save i with it

--- lib/auth.ts
+++ lib/auth.ts
@@ -13,7 +13,7 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, { provider: "postgresql" }),
   trustedOrigins: [
     process.env.BETTER_AUTH_URL
-      ? `https://${process.env.BETTER_AUTH_URL}`
+      ? `${process.env.BETTER_AUTH_URL}`
       : "http://localhost:3000",
   ],
   emailAndPassword: {
