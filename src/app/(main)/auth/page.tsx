2aab432276671997184212c27b5d8c17ab2908ac
Everett911 <everett.lara1697@gmail.com>
2026-08-25 19:25:26 +0800

    wrong ssr

--- src/app/(main)/auth/page.tsx
+++ src/app/(main)/auth/page.tsx
@@ -1,34 +1,11 @@
 import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
-import { Suspense } from "react";
-import { LoaderCircleIcon } from "lucide-react";
-import dynamic from "next/dynamic";
-
-const AuthClientPage = dynamic(() => import("./auth"), {
-  ssr: false,
-});
+import AuthClientPage from "./auth";
 
 export default async function AuthPage() {
   await auth.api.getSession({
     headers: await headers(),
   });
 
-  return (
-    <Suspense
-      fallback={
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh",
-          }}
-        >
-          <LoaderCircleIcon className="animate-spin" size={40} />
-        </div>
-      }
-    >
-      <AuthClientPage />
-    </Suspense>
-  );
+  return <AuthClientPage />;
 }
